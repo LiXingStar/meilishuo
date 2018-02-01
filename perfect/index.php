21a064e1332b72db27b129c171fc51121259ec52
lixing <1459294281@qq.com>
2018-02-01 18:11:19 +0800

    goods

--- perfect/index.php
+++ perfect/index.php
@@ -5,7 +5,6 @@ define('JS_PATH','/perfect/static/js/');
 define('IMG_PATH','/perfect/static/images/');
 define('ICON_PATH','/perfect/static/iconfont/');
 define('PLUG_PATH','/perfect/plug/');
-
 include 'libs/router.php';
 include 'libs/smarty/Smarty.class.php';
 include 'libs/main.php';
